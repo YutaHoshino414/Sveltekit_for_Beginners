18eeae9c2e6a9a9a615cabcdaa30821af2a7513b
Yuta Hoshino <y.hoshino25@gmail.com>
2022-04-04 20:22:28 +0900

    fetch data from moviedb & Add movie component

---
+++ src/components/MovieCard.svelte
@@ -0,0 +1,41 @@
+<script>
+  export let movie;
+</script>
+
+<div class="movie-card">
+  <img src={'https://image.tmdb.org/t/p/w500' + movie.poster_path } alt={movie.title} />
+  <div class="discription">
+    <h3>{movie.title}</h3>
+    <p>{movie.release_date}</p>
+  </div>
+</div>
+
+<style>
+  img {
+    width: 100%;
+    height: 350px;
+    
+    object-fit: cover;
+    border-radius: 1rem;
+    margin-bottom: 1rem;
+  }
+
+  h3 {
+    font-size: 0.9rem;
+  }
+
+  .description {
+    height: 5vh;
+  }
+
+  p{
+    font-size: 0.7rem;
+  }
+
+  .movie-card {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    padding: 1rem;
+  }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,23 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script context="module">
+  export async function load({ fetch }){
+    const resp = await fetch(
+      `https://api.themoviedb.org/3/movie/popular?api_key=${import.meta.env.VITE_API}&language=en-US&page=1`
+    );
+    const data = await resp.json();
+    console.log(data)
+    if(resp.ok){
+      return{
+        props:{ popular:data.results }
+      };
+    }
+  }
+</script>
+
+<script>
+  import PopularMovies from "../components/PopularMovies.svelte";
+  export let popular;
+</script>
+
+<section>
+  <PopularMovies {popular} />
+</section>
